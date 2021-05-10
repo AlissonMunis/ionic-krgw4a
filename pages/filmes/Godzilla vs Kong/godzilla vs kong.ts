4780a152164fa4104136f20b199f469e1d3c6bbc
AlissonMunis <alissonk413@gmail.com>
2021-05-10 09:04:58 -0300

    2 files added

---
+++ pages/filmes/Godzilla vs Kong/godzilla vs kong.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-godzilla vs kong',
+  templateUrl: 'godzilla vs kong.html'
+})
+export class GodzillaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
